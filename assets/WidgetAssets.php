8cd6684787434c2b1f660ac1765c99ae2541ed4b
alexandrr.naumenko <alexandrr.naumenko@gmail.com>
2017-01-12 12:08:14 +0200

    move assets menager

--- assets/WidgetAssets.php
+++ assets/WidgetAssets.php
@@ -1,11 +1,9 @@
-<?php namespace app\modules\friends\provider;
-
+<?php namespace app\modules\friends\assets;
 
 class WidgetAssets extends \yii\web\AssetBundle{
     
     public $sourcePath = '@app/modules/friends/assets';
     
     public $css = ['css/tabs.css'];
-    
-    
+
 }
