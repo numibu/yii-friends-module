98eb142b2575d267edbe76b3ca5013d115a2071b
alexandrr.naumenko <alexandrr.naumenko@gmail.com>
2017-01-12 12:05:03 +0200

    Add messages/en/ directory

---
+++ messages/en/friends.php
@@ -0,0 +1,17 @@
+<?php
+//EN
+return [
+    'ID' => 'id ',
+    'User ID' => 'user number',
+    'Full Name' => 'Full name',
+    'Name' => 'Name',
+    'Role' => 'Role',
+    'Status' => 'Status',
+    'Email' => 'E-mail',
+    'New Email' => 'new E-mail ',
+    'Users' => 'Users',
+    'Cancel' => 'Cancel',
+    'AllUser'=>'All users',//Всі активні користувачі
+    'friends'=>'Friends',//
+    'candidateFriends'=>'Candidates'//кандидати
+];
